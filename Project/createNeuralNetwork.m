9180c5f2b0d6cdc6e72ce79773709ad518040185
yuri0202 <31475047+yuri0202@users.noreply.github.com>
2018-11-17 18:08:22 +0100

    - Caricata la funzione per la creazione della rete neurale - Aggiornato il test sulla forward prop in relazione alla nuova funzione per creazione rete neurale

---
+++ Project/createNeuralNetwork.m
@@ -0,0 +1,84 @@
+function [ neuralNetwork ] = createNeuralNetwork( inputDimension, outputDimension, outputFunction, outputDerivative, hiddenLayers )
+% La funzione crea una rete neurale feed-forward multistrato
+%
+%   INPUT:
+%       - 'inputDimension': Numero di nodi dello strato di input
+%       - 'outputDimension': Numero di nodi dello strato di output
+%       - 'outputFunction': Funzione di attivazione dello strato di output
+%       - 'outputDerivative': Derivata della funzione di attivazione dello
+%                             strato di output
+%       - 'hiddenLayers': Array che rappresenta l'insieme degli hidden
+%                         layers. Ognuno di essi è così composto:
+%                         - 'size': numero di nodi
+%                         - outputFunction: Funzione di attivazione
+%                         - derivative: La derivata della funzione di
+%                         attivazione
+%
+%   OUTPUT:
+%       - 'neuralNetwork': Rete Neurale costituita dai seguenti campi:
+%           - 'inputDimension': Numero di nodi dello strato di input
+%           - 'outputDimension': Numero di nodi dello strato di output
+%           - 'hiddenLayersNum': Numero di hidden layers
+%           - 'n': Array tale per cui l'elemento i-esimo rappresenta il
+%                  numero di nodi presenti nel layer i
+%           - 'W': Array di matrici bidimensionali dove l'i-esimo elemento
+%                  rappresenta i pesi sulle connessioni tra il layer i e il
+%                  layer i-1 della rete
+%           - 'b': Array di matrici monodimensionali dove l'i-esimo
+%                  elemento rappresenta i valori dei bias del layer i
+%           - 'outputFunctions': Array tale per cui l'i-esimo elemento
+%                           rappresenta la funzione di attivazione del
+%                           layer i
+%           - 'derivativeFunctions ': Array tale per cui l'i-esimo elemento
+%                           rappresenta la derivata della funzione di
+%                           attivazione del layer i
+
+
+    % Controllo se gli elementi dell'array hiddenLayers sono rappresentati
+    % per riga o per colonna. Nel primo caso, effettuo la trasposizione
+    % dell'array per rispettare la notazione che prevede gli elementi
+    % rappresentati per colonne (per coerenza futura)
+    if size(hiddenLayers,1) > size(hiddenLayers,2)
+        hiddenLayers = hiddenLayers';
+    end
+    
+    neuralNetwork.inputDimension  = inputDimension;
+    neuralNetwork.outputDimension = outputDimension;
+    neuralNetwork.hiddenLayersNum = size(hiddenLayers,2);
+    
+    
+    % Riempio l'array n con il numero di nodi di ogni layer
+    for l = 1 : neuralNetwork.hiddenLayersNum
+        neuralNetwork.n(l) = hiddenLayers(l).size;
+    end
+    
+    % Generazione di pesi e bias tra lo strato di input e il primo hidden
+    % layer e assegnazione delle funzioni di attivazione e della sua derivata
+    neuralNetwork.W{1} = rand(hiddenLayers(1).size, inputDimension)-0.5;
+    neuralNetwork.b{1} = rand(1, hiddenLayers(1).size)-0.5;
+    neuralNetwork.outputFunctions{1} = hiddenLayers(1).function;
+    neuralNetwork.derivativeFunctions{1} = hiddenLayers(1).derivative;
+    
+    % Se vi è più di un hidden layer, allora generare pesi e bias per le
+    % connessioni tra questi ultimi e assegnare le funzioni di attivazione
+    % e la derivata per ogni hidden layer.
+    if(size(hiddenLayers,2)>1)
+        for i = 2 : size(hiddenLayers,2)
+            neuralNetwork.W{i} = rand(hiddenLayers(i).size, hiddenLayers(i-1).size);
+            neuralNetwork.b{i} = rand(1, hiddenLayers(i).size);
+            neuralNetwork.outputFunctions{i} = hiddenLayers(i).function;
+            neuralNetwork.derivativeFunctions{i} = hiddenLayers(i).derivative;
+        end
+    end
+    
+    % Generazione di pesi e bias tra l'ultimo hidden layer e lo strato di
+    % output e assegnazione delle funzioni di attivazione e della sua
+    % derivata
+    i = length(hiddenLayers) + 1;   
+    neuralNetwork.W{i} = rand(outputDimension, hiddenLayers(i-1).size)-0.5;
+    neuralNetwork.b{i} = rand(outputDimension, outputDimension)-0.5;
+    neuralNetwork.outputFunctions{i} = outputFunction;
+    neuralNetwork.derivativeFunctions{i} = outputDerivative;
+    
+end
+
--- Project/testForwardProp.m
+++ Project/testForwardProp.m
@@ -1,9 +1,11 @@
 % Script per testare la forward propagation
 
 % Creo la rete neurale
-net = createNeuralNetwork(2, 1, @identity, @identityDx, [
-    struct('size',2,'function',@sigmoid,'derivative',@sigmoidDx) % Hidden Layer
-],@derivativeSumOfSquares);
+net = createNeuralNetwork(5, 1, @sigmoid, @sigmoidDx, [
+    struct('size',5,'function',@sigmoid,'derivative',@sigmoidDx) % Hidden Layer
+    struct('size',5,'function',@sigmoid,'derivative',@sigmoidDx)
+    struct('size',5,'function',@identity,'derivative',@identityDx)
+]);
 
 
 disp('Pesi W1');
@@ -11,12 +13,11 @@ disp(net.W{1});
 disp('Bias liv. 1');
 disp(net.b{1});
 disp('Pesi W2');
-disp(net.W{2});
+disp(net.W{3});
 disp('Bias output');
-disp(net.b{2});
+disp(net.b{3});
 
-% Effettuo forward propagation
-[outputs, A] = forwardProp(net, [2,3]);
+[outputs, A] = forwardProp(net, [1,2,3,4,5]);
 
 disp('A strato interno');
 disp(A{1});
@@ -28,3 +29,4 @@ disp('Y');
 disp(outputs{2});
 
 disp('fine');
+
