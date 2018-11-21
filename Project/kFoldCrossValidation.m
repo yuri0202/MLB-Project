f427f0a443b021fb1fc24dfc355bb804e8f36148
yuri0202 <31475047+yuri0202@users.noreply.github.com>
2018-11-21 13:52:00 +0100

    Completato la funzione per la kfold, effettuato refactoring (divisa in due funzioni e aggiornato parametri di input e output), aggiunti i commenti

--- Project/kFoldCrossValidation.m
+++ Project/kFoldCrossValidation.m
@@ -1,124 +1,128 @@
-function [etaMinusBest,etaPlusBest, numHiddenNodesBest] = kFoldCrossValidation(trainingSetSize,epochs,outputFunction,outputFunctionDx,hiddenFunction,hiddenFunctionDx,supWeights,infWeights,errorFunction,errorFuctionDx,softMax,K,etaMins,etaPlus,numHiddenNodes, printFlag)
-% poi si vedrà
+function [Performances] = kFoldCrossValidation(trainingSetData, trainingSetLabels, trainingSetSize, foldSize,meanStdCurrentIndex, numIterations, K, HIDDEN_NODES, ETA_MINUS, ETA_PLUS, epochs, outputFunction, outputFunctionDx,hiddenFunction,hiddenFunctionDx,supWeights,infWeights,errorFunction,errorFuctionDx,softMax,printFlag);
+% Questa funzione effettua la K-Fold Cross Validation su una singola
+% combinazione di iper-parametri
+% INPUT:
+%   - 'trainingSetData': Set sul quale effettuare la K-Fold Cross
+%                        Validation
+%   - 'trainingSetLabels': target da ottenere rispetto alle immagini di
+%                          'trainingSetData'
+%   - 'trainingSetSize': Numero di elementi presenti in 'trainingSetData'
+%   - 'foldSize': Numero di elementi per ogni fold (trainingSetSize/K)
+%   - 'meanStdCurrentIndex': Indice che tiene traccia dell'ultimo elemento
+%                            inserito nella matrice meanStdCurrentIndex per
+%                            il calcolo della media e della deviazione
+%                            standard delle performances
+% - 'numIterations': Numero totale di iterazioni da effettuare, dato dalla
+%                    moltiplicazione delle cardinalità dell'array di
+%                    possibili valori per etaMinus, etaPlus e Numero di
+%                    nodi dello strato interno
+%   - 'K': Numero di Fold con cui effettuare la K-Fold
+%   - 'HIDDEN_NODES': Il numero di nodi dello strato interno per la
+%                     combinazione corrente di iper-parametri
+%   - 'ETA_MINUS': Il valore di etaMinus per la
+%                  combinazione corrente di iper-parametri
+%   - 'ETA_PLUS': Il valore di etaPlus per la
+%                  combinazione corrente di iper-parametri
+%   - 'epochs': Numero massimo di epoche con cui addestrare la rete
 
-    % Controlla che trainingSetSize sia multiplo di K 
-    if (mod(trainingSetSize, K) ~= 0) 
-        error('Il numero di elementi su cui effettuare la K-fold cross validation deve essere multiplo di K');
-        return
-    end
+%   - 'outputFunction': Funzione di attivazione dello strato di output
+%   - 'outputDerivative': Derivata della funzione di attivazione dello
+%                         strato di output
+%   - 'hiddenFunction': Funzione di attivazione dello strato interno
+%   - 'hiddenFunctionDx': Derivata della funzione di attivazione dello
+%                         strato interno
+%   - 'supWeights': Estremo superiore dell'intervallo dei valori con cui
+%                   riempire casualmente i pesi della rete.
+%   - 'infWeights': Estremo inferiore dell'intervallo dei valori con cui
+%                   riempire casualmente i pesi della rete.
+%   - 'errorFunction': Funzione da utilizzare per il calcolo dell'errore
+%   - 'errorFunctionDx': Derivata della funzione da utilizzare per il
+%                        calcolo dell'errore
+%   - 'softmax': Se uguale a TRUE, all'output della rete (dopo la forward
+%                propagation) verrà applicato il softmax; no altrimenti
+%   - 'printFlag': Se uguale a TRUE, verranno stampati a video i valori
+%                  degli errori calcolati rispetto al Training e al
+%                  Validation Set durante la fase di RProp
 
-    % Carico il training set del dataset MNIST
-    [trainImages, trainLabels] = loadMNIST('./mnist/train-images-idx3-ubyte', './mnist/train-labels-idx1-ubyte');
-   
-    
-    [trainingSetData, trainingSetLabels, ~, ~, ~, ~] = createSets(trainImages', trainLabels, trainImages', trainLabels, trainingSetSize, 0, 0);
-    %trainingSetData = [1 1 1 1 1 1 1 1 1 1; 2 2 2 2 2 2 2 2 2 2; 3 3 3 3 3 3 3 3 3 3; 4 4 4 4 4 4 4 4 4 4; 5 5 5 5 5 5 5 5 5 5; 6 6 6 6 6 6 6 6 6 6; 7 7 7 7 7 7 7 7 7 7; 8 8 8 8 8 8 8 8 8 8; 9 9 9 9 9 9 9 9 9 9; 10 10 10 10 10 10 10 10 10 10];
-    foldSize = trainingSetSize/K;
-    
-    % PROBABILMENTE BISOGNA SHUFFLARE LE RIGHE DELLA MATRICE O SI AVRANNO
-    % RISULTATI MEH perchè la funzione createSets restituisce i valori in ordine di
-    % cifra
-    
-    shuffle   = randperm(trainingSetSize);
-    trainingSetData   = trainingSetData(shuffle,:);
-    trainingSetLabels = trainingSetLabels(shuffle,:);
-    
+    % Vengono definiti gli indici iniziali per la prima
+    % partizione del set in Ts, Vs e Tes
+    TsStart = 1;
+    TsEnd = (K-2)*foldSize;
+    VsStart = (K-2)*foldSize+1;
+    VsEnd = (K-1)*foldSize;
+    TesStart = (K-1)*foldSize +1;
+    TesEnd = trainingSetSize;
 
+    % Array dove salvo le performances per ogni iterazione del
+    % K-Fold 
+    Performances= zeros(1,K);
     
-    % Matrice che conserva per ogni combinazione di valori degli
-    % iper-parametri, la media delle performance ottenute tramite k-fold
-    % cross validation
-    avgPerComb = zeros(size(etaMins,1)*size(etaPlus,1)*size(numHiddenNodes,1),4);
-    
-    % Inizio le iterazioni sui possibili valori per gli iper-parametri
-    for i=1 : size(numHiddenNodes,2)     
-        for j = 1 : size(etaMins,2)
-            for z = 1 : size(etaPlus,2)
-                % Assegno gli iper-parametri di questa iterazione
-                ETA_MINUS = etaMins(j);
-                ETA_PLUS = etaPlus(z);
-                
-                % Probabilmente questa parte qui metterla in una sola
-                % funzione che chiamiamo kFoldCrossValidation mentre questa
-                % funzione qui la chiamiamo tuneHyperParametersCV o qualcosa
-                % del genere
-                
-                % Vengono definiti gli indici iniziali per la prima
-                % partizione del set in Ts, Vs e Tes
-                TsStart = 1;
-                TsEnd = (K-2)*foldSize;
-                VsStart = (K-2)*foldSize+1;
-                VsEnd = (K-1)*foldSize;
-                TesStart = (K-1)*foldSize +1;
-                TesEnd = trainingSetSize;
-                
-                % Array dove salvo le performances per ogni iterazione del
-                % k-fold (se facciamo funzione interna questo array verrà
-                % ritornato dalla funzione)
-                Performances= zeros(1,K);
-                for k = 1 : K
-                    % Se il Training Set inizia in un fold < K e finisce in
-                    % un fold > K (Esempio[K=10] Training Set = folds 4 5 6 7
-                    % 7 9 10 1 2), allora sarà ovviamente composto dagli
-                    % elementi che vanno dal fold 4 al fold K e quelli dal
-                    % fold 1 al fold 2
-                    if TsStart > TsEnd
-                        TSData = trainingSetData([TsStart:trainingSetSize,1:TsEnd],:);
-                        TSLabels = trainingSetLabels([TsStart:trainingSetSize,1:TsEnd],:);
-                    else
-                        TSData = trainingSetData(TsStart:TsEnd,:);
-                        TSLabels = trainingSetLabels(TsStart:TsEnd,:);
-                    end
-                    VSData = trainingSetData(VsStart:VsEnd,:);
-                    VSLabels = trainingSetLabels(VsStart:VsEnd,:);
-                    TeSData = trainingSetData(TesStart:TesEnd,:);             
-                    TeSLabels = trainingSetLabels(TesStart:TesEnd,:);
-                    
-                            
-                    % Creo la rete neurale con numero di nodi dell'hidden layer pari a
-                    % numHiddenNodes(i)
-                    HIDDEN_NODES = numHiddenNodes(i);
-                    net = createNeuralNetwork(size(trainingSetData,2), size(trainingSetLabels,2), outputFunction, outputFunctionDx, [
-                        struct('size',HIDDEN_NODES,'function',hiddenFunction,'derivative',hiddenFunctionDx) % Hidden Layer
-                    ], supWeights,infWeights);
-                    
-                    % Addestro la rete con la combinazione di parametri di
-                    % questa iterazione su Training e Validation
-                    [net, trainingSetErrors, validationSetErrors] = trainNeuralNetworkRProp(net, TSData, TSLabels, VSData, VSLabels, epochs, errorFunction,errorFuctionDx, ETA_MINUS, ETA_PLUS, softMax, printFlag);
-                    
-                    % Calcolo l'accuracy
-                    [output, ~] = forwardProp(net,TeSData, true);
-                    [totalAccuracy] = evaluateNetClassifier(output{net.hiddenLayersNum+1}, TeSLabels);
-                    totalAccuracy = totalAccuracy*100;
-                    Performances(k) = totalAccuracy;
-                    fprintf("\n Accuracy della rete neurale, Itarazione kfold %d: %.2f%% \n", k,totalAccuracy);
-                    
-                    % 5) Calcolo la media e deviazione standard salvo la media dentro
-                    %    avgPerComb con i 3 parametri di riferimento
-                    % 6) Se la media è minore della media migliore fino ad
-                    % ora, aggiorna il valore dei parametri migliori
-                    
-                    % Aggiorno gli indici per la prossima iterazione
-                    % Utilizzo la funzione realizzata myMod tale che dati
-                    % due numeri a e b, restituisce il mod(a,b) se a > b.
-                    % Sostanzialmente è uguale alla funzione mod tranne per
-                    % il fatto che il mod(x,x) = x e non 0
-                    TsStart = myMod(TsStart+foldSize,trainingSetSize);
-                    TsEnd = myMod(TsEnd+foldSize,trainingSetSize);
-                    VsStart = myMod(VsStart+foldSize,trainingSetSize);
-                    VsEnd = myMod(VsEnd+foldSize,trainingSetSize);
-                    TesStart = myMod(TesStart+foldSize,trainingSetSize);
-                    TesEnd = myMod(TesEnd+foldSize,trainingSetSize);
-      
-                end                
-            end
+    fprintf("\nParametri del K-fold corrente: numero nodi = %d, etaMinus =%.2f, etaPlus = %.2f\n", HIDDEN_NODES,ETA_MINUS,ETA_PLUS);
+    fprintf("Iterazione %d di %d\n\n",meanStdCurrentIndex,numIterations);
+    for k = 1 : K
+        % Split in TS, Vs e TeS in accordo con gli indici calcolati
+        % all'iterazione precedente
+
+        if TsStart > TsEnd
+            % Se l'indice di fine set del Training Set è maggiore dell'indice
+            % di inizio set (Esempio[K=10] Training Set = folds 4 5 6 7
+            % 7 9 10 1 2), allora il TS sarà ovviamente composto dagli
+            % elementi che vanno dal fold 4 al fold K e quelli dal
+            % fold 1 al fold 2
+            TSData = trainingSetData([TsStart:trainingSetSize,1:TsEnd],:);
+            TSLabels = trainingSetLabels([TsStart:trainingSetSize,1:TsEnd],:);
+        else
+            TSData = trainingSetData(TsStart:TsEnd,:);
+            TSLabels = trainingSetLabels(TsStart:TsEnd,:);
         end
+        VSData = trainingSetData(VsStart:VsEnd,:);
+        VSLabels = trainingSetLabels(VsStart:VsEnd,:);
+        TeSData = trainingSetData(TesStart:TesEnd,:);
+        TeSLabels = trainingSetLabels(TesStart:TesEnd,:);
+
+
+        % Creo la rete neurale con numero di nodi dell'hidden layer pari a
+        % HIDDEN_NODES
+
+        net = createNeuralNetwork(size(trainingSetData,2), size(trainingSetLabels,2), outputFunction, outputFunctionDx, [
+            struct('size',HIDDEN_NODES,'function',hiddenFunction,'derivative',hiddenFunctionDx) % Hidden Layer
+            ], supWeights,infWeights);
+
+        % Addestro la rete con la combinazione di parametri di
+        % questa iterazione su Training e Validation
+        [net,~, ~] = trainNeuralNetworkRProp(net, TSData, TSLabels, VSData, VSLabels, epochs, errorFunction,errorFuctionDx, ETA_MINUS, ETA_PLUS, softMax, printFlag);
+
+        % Forward propagation della rete addestrata utilizzando come input
+        % il test set di questa iterazione di K-Fold
+        [output, ~] = forwardProp(net,TeSData, true);
+        
+        % Calcolo dell'accuratezza delle risposte della rete, confrontandole con le
+        % label effettive del test set.
+        [totalAccuracy] = evaluateNetClassifier(output{net.hiddenLayersNum+1}, TeSLabels);
+        totalAccuracy = totalAccuracy*100;
+        Performances(k) = totalAccuracy;
+        fprintf("Accuracy della rete neurale - Itarazione kfold %d di %d: %.2f%%\n", k,K,totalAccuracy);
+
+        % Aggiorno gli indici per la prossima iterazione
+        % Utilizzo la funzione realizzata myMod tale che dati
+        % due numeri a e b, restituisce il mod(a,b) solo se a > b.
+        % Sostanzialmente è uguale alla funzione mod tranne per
+        % il fatto che il mod(x,x) = x e non 0
+        TsStart = myMod(TsStart+foldSize,trainingSetSize);
+        TsEnd = myMod(TsEnd+foldSize,trainingSetSize);
+        VsStart = myMod(VsStart+foldSize,trainingSetSize);
+        VsEnd = myMod(VsEnd+foldSize,trainingSetSize);
+        TesStart = myMod(TesStart+foldSize,trainingSetSize);
+        TesEnd = myMod(TesEnd+foldSize,trainingSetSize);
     end
-    
 end
 
 
 function [myMod] = myMod (number,module)
+% Questa funzione, utilizzata durante l'aggiornamento degli indici per i
+% vari set tra una iterazione e l'altra della K-Fold Cross Validation, è
+% una modifica alla classica funzione di modulo. Si differenzia dal fatto
+% che myMod(x,x) = x e non 0 come nella classica funzione di mod.
     
     if (number == module)
         myMod = number;
---
+++ Project/modelHyperParametersOptimization.m
@@ -0,0 +1,113 @@
+function [meanStdPerComb] = modelHyperParametersOptimization(trainImages,trainLabels,trainingSetSize,epochs,outputFunction,outputFunctionDx,hiddenFunction,hiddenFunctionDx,supWeights,infWeights,errorFunction,errorFuctionDx,softMax,K,etaMins,etaPlus,numHiddenNodes, printFlag)
+% Questa funzione trova la combinazione di parametri ottimi (etaMinus,
+% etaPlus e numero di nodi dello strato interno) per l'addestramento 
+% della rete, sfruttando la tecnica del K-Fold Cross Validation
+
+%
+% INPUT:
+%   - 'trainImages': Matrice di immagini dal training set di MNIST
+%                    (60000x784), ottenuta dalla funzione 'loadMNIST'
+%   - 'trainLabels': Matrice di labels dal training set di MNIST (60000x1),
+%                    ottenuta dalla funzione 'loadMNIST' 
+%   - 'trainingSetSize': Numero di elementi da prendere da trainImages sul
+%                        quale effettuare la K-Fold
+%   - 'epochs': Numero massimo di epoche con cui addestrare la rete
+
+%   - 'outputFunction': Funzione di attivazione dello strato di output
+%   - 'outputDerivative': Derivata della funzione di attivazione dello
+%                         strato di output
+%   - 'hiddenFunction': Funzione di attivazione dello strato interno
+%   - 'hiddenFunctionDx': Derivata della funzione di attivazione dello
+%                         strato interno
+%   - 'supWeights': Estremo superiore dell'intervallo dei valori con cui
+%                   riempire casualmente i pesi della rete.
+%   - 'infWeights': Estremo inferiore dell'intervallo dei valori con cui
+%                   riempire casualmente i pesi della rete.
+%   - 'errorFunction': Funzione da utilizzare per il calcolo dell'errore
+%   - 'errorFunctionDx': Derivata della funzione da utilizzare per il
+%                        calcolo dell'errore
+%   - 'softmax': Se uguale a TRUE, all'output della rete (dopo la forward
+%                propagation) verrà applicato il softmax; no altrimenti
+%   - 'K': Numero di Fold con cui effettuare la K-Fold
+%   - 'etaMins': Array che contiene i diversi valori di etaMin con le quali
+%                provare le diverse combinazioni
+%   - 'etaPlus': Array che contiene i diversi valori di etaPlus con le quali 
+%                provare le diverse combinazioni
+%   - 'numHiddenNodes': Array che contiene i diversi valori di numero di
+%                       nodi interni con i quali provare le diverse
+%                       combinazioni
+%   - 'printFlag': Se uguale a TRUE, verranno stampati a video i valori
+%                  degli errori calcolati rispetto al Training e al
+%                  Validation Set durante la fase di RProp
+
+% OUTPUT:
+%   - 'meanStdPerComb': Matrice che, per ogni combinazione di parametri,
+%                       conserva la media e la deviazione standard delle
+%                       performances ottenute nelle K iterazioni del
+%                       processo di K-Fold Cross Validation
+
+    % Controlla che trainingSetSize sia multiplo di K 
+    if (mod(trainingSetSize, K) ~= 0) 
+        error('Il numero di elementi su cui effettuare la K-fold cross validation deve essere multiplo di K');
+        return
+    end
+   
+    
+    % Estraggo elementi random e distinti dal training set MNIST
+    [trainingSetData, trainingSetLabels, ~, ~, ~, ~] = createSets(trainImages', trainLabels, trainImages', trainLabels, trainingSetSize, 0, 0);
+    %trainingSetData = [1 1 1 1 1 1 1 1 1 1; 2 2 2 2 2 2 2 2 2 2; 3 3 3 3 3 3 3 3 3 3; 4 4 4 4 4 4 4 4 4 4; 5 5 5 5 5 5 5 5 5 5; 6 6 6 6 6 6 6 6 6 6; 7 7 7 7 7 7 7 7 7 7; 8 8 8 8 8 8 8 8 8 8; 9 9 9 9 9 9 9 9 9 9; 10 10 10 10 10 10 10 10 10 10];
+    foldSize = trainingSetSize/K;
+    
+    % TODO
+    % PROBABILMENTE BISOGNA SHUFFLARE LE RIGHE DELLA MATRICE O SI AVRANNO
+    % RISULTATI MEH perchè la funzione createSets restituisce i valori in ordine di
+    % cifra (Alternativa è creare una funzione che prende in input
+    % trainingSetData e trainingSetLabels e bilancia il tutto (es. invece di
+    % avere 111,222,333 si avrà 123,123,123)
+    
+    % Effettuo una permutazione random degli elementi di trainingSetData e
+    % trainingSetLabels
+    shuffle   = randperm(trainingSetSize);
+    trainingSetData   = trainingSetData(shuffle,:);
+    trainingSetLabels = trainingSetLabels(shuffle,:);
+    
+
+    
+    % Definisco la matrice che conserva per ogni combinazione di valori degli
+    % iper-parametri, la media e la deviazione standard delle performance 
+    % ottenute tramite k-fold cross validation e l'indice che mi tiene 
+    % traccia dell'ultimo elemento inserito
+    numIterations = size(etaMins,2)*size(etaPlus,2)*size(numHiddenNodes,2);
+    meanStdPerComb = zeros(numIterations,5);
+    meanStdCurrentIndex = 1;
+    
+   
+    % Inizio le iterazioni sui possibili valori per gli iper-parametri
+    for i=1 : size(numHiddenNodes,2)    
+        HIDDEN_NODES = numHiddenNodes(i);
+        for j = 1 : size(etaMins,2)
+            ETA_MINUS = etaMins(j);
+            for z = 1 : size(etaPlus,2)
+                ETA_PLUS = etaPlus(z);
+                
+                % Eseguo la K-Fold Cross Validation con la combinazione di
+                % parametri corrente e ne salvo le performances per ogni
+                % K-iterazione
+                Performances = kFoldCrossValidation(trainingSetData, trainingSetLabels, trainingSetSize, foldSize, meanStdCurrentIndex, numIterations, K, HIDDEN_NODES, ETA_MINUS, ETA_PLUS, epochs, outputFunction, outputFunctionDx,hiddenFunction,hiddenFunctionDx,supWeights,infWeights,errorFunction,errorFuctionDx,softMax,printFlag);
+
+                % Aggiorno la matrice meanStdPerComb con il valore medio e
+                % la deviazione standard di tutte le performances calcolate
+                % nelle K iterazioni
+                meanStdPerComb(meanStdCurrentIndex,1) = HIDDEN_NODES;
+                meanStdPerComb(meanStdCurrentIndex,2) = ETA_MINUS;
+                meanStdPerComb(meanStdCurrentIndex,3) = ETA_PLUS;
+                meanStdPerComb(meanStdCurrentIndex,4) = mean(Performances);
+                meanStdPerComb(meanStdCurrentIndex,5) = std(Performances,1); % Divido per N e non uso la correzione di Bessel
+                meanStdCurrentIndex = meanStdCurrentIndex +1;
+            end
+        end
+    end  
+end
+
+
+
